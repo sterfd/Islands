8fac88d2540778883f8b9dd0cfd7d446699f0cd7
sterf-mac <stefhwin@gmail.com>
2023-08-08 12:39:37 -0400

    map board squares to starting board and display island areas

--- src/App.js
+++ src/App.js
@@ -2,55 +2,62 @@ import React from 'react';
 import { useState } from 'react';
 
 const startingBoard = [
-  [1, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0],
-  [0, 0, 3, 0, 0],
-  [0, 3, 0, 1, 0],
-  [0, 0, 0, 0, 0]
+  [5, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 3, 1, 1],
+  [1, 3, 1, 5, 1],
+  [1, 1, 1, 1, 1]
 ];
 
 const solutionBoard = [
-  [1, 0, 0, 0, 0],
+  [5, 0, 0, 0, 0],
   [0, 0, 1, 0, 0],
   [1, 0, 3, 0, 0],
-  [1, 3, 0, 1, 0],
+  [1, 3, 0, 5, 0],
   [0, 0, 0, 0, 0]
 ];
 
 function Square({ value, onSquareClick }) {
   if (value === 0) {
     return <button
-      className='empty' onClick={onSquareClick}>{value}</button>;
+      className='filled' onClick={onSquareClick}></button>;
+  } else if (value === 1) {
+    return <button
+      className='empty' onClick={onSquareClick}></button>;
+  } else if (value === 5) {
+    return <button
+      className='empty' onClick={onSquareClick}>1</button>;
   } else {
     return <button
-      className='filled' onClick={onSquareClick}></button>;
+      className='empty' onClick={onSquareClick}>{value}</button>;
   }
 }
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(5).fill(0));
 
-  function handleClick(i) {
+export default function Board() {
+  const [squares, setSquares] = useState(startingBoard);
+
+  function handleClick(row, col) {
     const nextSquares = [...squares];
-    if (squares[i] === 0) {
-      nextSquares[i] = 1;
-    } else {
-      nextSquares[i] = 0;
+    if (squares[row][col] === 0) {
+      nextSquares[row][col] = 1;
+    } else if (squares[row][col] === 1) {
+      nextSquares[row][col] = 0;
     }
     setSquares(nextSquares);
   }
 
   return (
     <div>
-      <div className='board-row'>
-        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-      </div>
+      {squares.map((row, rowIndex) => (
+        <div className='board-row' key={rowIndex}>
+          {row.map((value, colIndex) => (
+            <Square key={colIndex} value={value} onSquareClick={() => handleClick(rowIndex, colIndex)} />
+          ))}
+        </div>
+      ))}
     </div>
-  )
+  );
 }
 
 

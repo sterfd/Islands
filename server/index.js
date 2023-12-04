dc93a10344cf883683209d8f1b666c0f49fdca4a
sterf-mac <stefhwin@gmail.com>
2023-12-04 10:36:06 -0500

    dynamic port number

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const server = require('./api/server')
 
 const HOST = 'localhost';
-const PORT = 8888;
+const PORT = process.env.PORT || 8888;
 
 server.listen(PORT, () => console.log(`Server Running at ${HOST}:${PORT}.`)); // starts server 
 

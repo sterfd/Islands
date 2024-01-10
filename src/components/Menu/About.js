7dc1da7f63d2918e845a0c7054a0d7a8b24efd48
sterf-mac <stefhwin@gmail.com>
2024-01-10 16:16:58 -0500

    updated about page

--- src/components/Menu/About.js
+++ src/components/Menu/About.js
@@ -1,28 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
 
 export default function About() {
-    async function getRequest() {
-        const response = await axios.get('http://localhost:8888/');
-        console.log(response);
-    }
-
-    async function getAllGames() {
-        const response = await axios.get('http://localhost:8888/allgames');
-        console.log(response);
-    }
-
-
-
-
-
 
     return (
         <div className='sub-menu'>
             <p>This game is based on Nurikabe by the publisher Nikoli!</p>
             <p>A fun project made in React by sterfd.</p>
-            <button onClick={() => getAllGames()}>Getallgames</button>
             <Link className='main' style={{ textDecoration: 'none' }} to='/'>Main Menu
             </Link>
         </div>
